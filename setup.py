650dd3ef6bce9f8d3fbd0e3bad04b96df269addc
Rafoolin <niloo.jv@gmail.com>
2023-11-13 23:40:08 +0100

    Math Quiz version 0.1.0
    Add setup file and create package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math_quiz",
+    version="0.1.0",
+    author="Rafoolin",
+    author_email="niloo.jv@gmail.com",
+    description="DSSS Homework 02!",
+    long_description="A Math quiz for FAU DSSS WS 2023/2024",
+    url="https://github.com/rafoolin/dsss_homework_02",
+    packages=find_packages(),
+    install_requires=[],
+)
